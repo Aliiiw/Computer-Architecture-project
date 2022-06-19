c7f469b14364d2e4e9332865f294d024bbae1e81
Ali Rahimi <1aliirahimi1@gmail.com>
2022-06-19 22:07:12 +0430

    update main

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy
+import InstructionMemory
+import PC
+import BinaryToDecimal
+address = []
+a = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+i = 0
+ClockCycle = 0
+file = open("instructions.txt", "r")
+for each in file: 
+    if(each!='\n'):
+        address.append(each)
+file.close()
+
+pc = "00000000000000000000000000000000"
+while(True):
+
+    i = BinaryToDecimal.BinaryToDecimal(pc).BinaryToDecimal()
+    i = i // 4
+
+    ins="00000000000000000000000000000000"
+    instruction = InstructionMemory.InstructionMemory(ins, a, ClockCycle, pc).fetch()
+    ClockCycle += 1
+    for j in range(5):
+        print(a[j][0],a[j][1])
+    print(ClockCycle)
+    print('\n\n')
+    if (instruction == "Undefined opcode"):
+        print("Undefined opcode")
+        break
+    elif(instruction == "J"):
+        pc = a[0][5]
+    elif(instruction != "Nope"):
+        pc = PC.PC(pc).PC_Add_By_four()
