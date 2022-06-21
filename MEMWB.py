1efa6a50b092c850ea661bfbacfb1ded0adbc4d8
Lari <zamani13816@gmail.com>
2022-06-21 16:06:09 +0430

    mission completed

---
+++ MEMWB.py
@@ -0,0 +1,12 @@
+import WriteBack
+import Forwarding
+class MEMWB:
+    def __init__(self,array,counter):
+        self.array=array
+        self.counter=counter
+    def call(self):
+        if(self.counter*5>=20):
+            WriteBack.WriteBack(self.array[4][2],self.array[4][0],self.array,self.counter).making_decition()
+    def continues(self):
+        WriteBack.WriteBack(self.array[4][2],self.array[4][0],self.array,self.counter).making_decition()
+        
