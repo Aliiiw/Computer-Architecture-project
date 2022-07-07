a27b6e830ae61c48b8abce43c6df62e1677a15d1
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-07 14:42:30 +0430

    test db

--- Memory.py
+++ Memory.py
@@ -1,61 +1,45 @@
 import psycopg2, psycopg2.extras
 
-host_name = "185.105.239.28"
-database_name = "camputerArchitecture"
-root_user = "aliiiw"
-root_password = "qazwsx"
-port_id = 5432
-    
-connection_to_database = psycopg2.connect(
-host=host_name,
-dbname=database_name,
-user=root_user,
-password=root_password,
-port=port_id)
-
-open_connection = connection_to_database.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-sql_insert_script = "insert into dataMemory (address,  string_data) values(%s, %s);"
-sql_select_script = "select * from dataMemory;"
-insert_value = (2, '87654321')
 #open_connection.execute(sql_insert_script, insert_value)
-sql_update_script = "update dataMemory set string_data = %s where address = %s"
-update_value = ('00000000', 1)
-open_connection.execute(sql_update_script, update_value)
 
-#open_connection.execute(sql_select_script)
-#result = open_connection.fetchall()
 
 # for record in result:
 #     address = record['address']
 #     string_data = record['string_data']
 #     print(address, string_data)
 
-
-connection_to_database.commit()
-
-
-open_connection.close()
-    
 def Memory(address ,data, read , write , Type) :
     
-    # host_name = "185.105.239.28"
-    # database_name = "camputerArchitecture"
-    # root_user = "aliiiw"
-    # root_password = "qazwsx"
-    # port_id = 5432
+    host_name = "185.105.239.28"
+    database_name = "camputerArchitecture"
+    root_user = "aliiiw"
+    root_password = "qazwsx"
+    port_id = 5432
     
-    # connection_to_database = psycopg2.connect(
-    # host=host_name,
-    # dbname=database_name,
-    # user=root_user,
-    # password=root_password,
-    # port=port_id)
+    connection_to_database = psycopg2.connect(
+    host=host_name,
+    dbname=database_name,
+    user=root_user,
+    password=root_password,
+    port=port_id)
+    
+    open_connection = connection_to_database.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    sql_insert_script = "insert into dataMemory (address,  string_data) values(%s, %s);"
+   
+    insert_value = (3, '22222222')
+    #open_connection.execute(sql_insert_script, insert_value)
+    sql_update_script = "update dataMemory set string_data = %s where address = %s"
+    update_value = ('33333333', 3)
+    open_connection.execute(sql_update_script, update_value)
+    
+    
+    connection_to_database.commit()
 
-    # open_connection = connection_to_database.cursor()
+
+    
+
 
     # sql_insert_script = "insert into dataMemory values(default, %s, %s);"
-    # sql_select_script = "insert into dataMemory values(default, %s, %s);"
     # insert_value = ('default', 1, '12345678')
     # open_connection.execute(sql_insert_script, insert_value)
     # connection_to_database.commit()
@@ -69,8 +53,20 @@ def Memory(address ,data, read , write , Type) :
     
     if read == '1' :
         
+        
+
         Memdata = "" 
         for i in range(size) :
+            
+            sql_select_script = "select * from dataMemory where address = %s;"
+            select_value = (address + i)
+            open_connection.execute(sql_select_script, select_value)
+            result = open_connection.fetchall()
+            for element in result:
+                print(element['string_data'])
+            
+            
+            
             Memdata += DB [address + i ]
         
         if signed :
@@ -85,3 +81,5 @@ def Memory(address ,data, read , write , Type) :
             DB [address + i ] = data [i*8:i*8+8]
         for i in range(size-1,-1,-1) :
             DB [address + i ] = data [-(i+1)*8 :- (i)*8]
+            
+    open_connection.close()
